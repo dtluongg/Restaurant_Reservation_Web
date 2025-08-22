90cbf1aa11fd9a81fde03a8677116af135c7ad5a
dtluongg <fastdominicracing@gmail.com>
2025-08-22 19:57:33 +0700

    Update: Complete connect frontend with backend

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -3,10 +3,15 @@ import { Routes, Route } from 'react-router-dom'
 import Navbar from './component/Navbar'
 import Footer from './component/Footer'
 import Homepage from './pages/Homepage' 
+import { ToastContainer } from 'react-toastify'
+
+export const backendUrl = "http://localhost:4000"
+
 
 const App = () => {
   return (
     <div>
+      <ToastContainer />
       <Navbar />
       <Routes>
         <Route path='/' element={<Homepage />} />
